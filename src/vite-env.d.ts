555e8306e6a17f5a1c4ccb9e1a27b434a8107b77
Pierre-Luc Fortin <pierre-luc.fortin@bzinc.ca>
2025-12-12 12:04:11 -0500

    fix: Restauration de vite-env.d.ts pour les dÃ©clarations de types des assets

---
+++ src/vite-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+
+// Déclarations de types pour les fichiers d'assets
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
